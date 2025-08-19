1c21c15b29352016be20c61181d76ab0913f3538
drod1107 <davidrodrigueznolimit@gmail.com>
2025-08-19 14:19:36 -0500

    restore errantly removed tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    typography,
+  ],
+};
+
+export default config;
